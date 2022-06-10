7bbf2ac512ee0dc5ed822c0f56d0825407d7f6b5
VinuSundar <vinusundar4@gmail.com>
2022-06-11 03:35:32 +0530

    Added code n contact page

--- src/main/java/com/pages/Contact.java
+++ src/main/java/com/pages/Contact.java
@@ -11,4 +11,23 @@ this.driver=driver;
 
 	}
 
+	public String gettitle()
+	{
+	
+		String title = driver.getTitle();
+		return title;
+	}
+
+
+	
+	public void enterusername(String us)
+	{
+		driver.findElement(username).sendKeys(us);
+	}
+
+	public void enterpassword(String pass)
+	{
+		driver.findElement(password).sendKeys(pass);
+	
+
 }
