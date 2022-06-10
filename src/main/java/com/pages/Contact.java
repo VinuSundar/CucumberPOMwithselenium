b094a45f701ff209069ead0619e7013064596e3a
VinuSundar <vinusundar4@gmail.com>
2022-06-11 01:30:05 +0530

    Added Contact.java

---
+++ src/main/java/com/pages/Contact.java
@@ -0,0 +1,14 @@
+package com.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.factory.DriverFactory;
+
+public class Contact {
+	WebDriver driver;
+	public Contact(WebDriver driver) {
+this.driver=driver;
+
+	}
+
+}
